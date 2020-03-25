9d3d996a3f6959e3594ae8a4a7a0b4e5eafd1144
Trent Nelson <trent@solana.com>
2020-03-25 14:28:53 -0600

    Cosmetics: Fix bad indent

--- libsol/instruction_test.c
+++ libsol/instruction_test.c
@@ -31,42 +31,42 @@ void test_instruction_program_id_unknown() {
 }
 
 void test_instruction_validate_ok() {
-  uint8_t accounts[] = {1, 2, 3};
-  Instruction instruction = {0, accounts, 3, NULL, 0};
-  MessageHeader header = {{0, 0, 0, 4}, NULL, NULL, 1};
-  assert(instruction_validate(&instruction, &header) == 0);
+    uint8_t accounts[] = {1, 2, 3};
+    Instruction instruction = {0, accounts, 3, NULL, 0};
+    MessageHeader header = {{0, 0, 0, 4}, NULL, NULL, 1};
+    assert(instruction_validate(&instruction, &header) == 0);
 }
 
 void test_instruction_validate_bad_program_id_index_fail() {
-  uint8_t accounts[] = {1, 2, 3};
-  Instruction instruction = {4, accounts, 3, NULL, 0};
-  MessageHeader header = {{0, 0, 0, 4}, NULL, NULL, 1};
-  assert(instruction_validate(&instruction, &header) == 1);
+    uint8_t accounts[] = {1, 2, 3};
+    Instruction instruction = {4, accounts, 3, NULL, 0};
+    MessageHeader header = {{0, 0, 0, 4}, NULL, NULL, 1};
+    assert(instruction_validate(&instruction, &header) == 1);
 }
 
 void test_instruction_validate_bad_first_account_index_fail() {
-  uint8_t accounts[] = {4, 2, 3};
-  Instruction instruction = {0, accounts, 3, NULL, 0};
-  MessageHeader header = {{0, 0, 0, 4}, NULL, NULL, 1};
-  assert(instruction_validate(&instruction, &header) == 1);
+    uint8_t accounts[] = {4, 2, 3};
+    Instruction instruction = {0, accounts, 3, NULL, 0};
+    MessageHeader header = {{0, 0, 0, 4}, NULL, NULL, 1};
+    assert(instruction_validate(&instruction, &header) == 1);
 }
 
 void test_instruction_validate_bad_last_account_index_fail() {
-  uint8_t accounts[] = {1, 2, 4};
-  Instruction instruction = {0, accounts, 3, NULL, 0};
-  MessageHeader header = {{0, 0, 0, 4}, NULL, NULL, 1};
-  assert(instruction_validate(&instruction, &header) == 1);
+    uint8_t accounts[] = {1, 2, 4};
+    Instruction instruction = {0, accounts, 3, NULL, 0};
+    MessageHeader header = {{0, 0, 0, 4}, NULL, NULL, 1};
+    assert(instruction_validate(&instruction, &header) == 1);
 }
 
 int main() {
-  test_instruction_validate_ok();
-  test_instruction_validate_bad_program_id_index_fail();
-  test_instruction_validate_bad_first_account_index_fail();
-  test_instruction_validate_bad_last_account_index_fail();
-  test_instruction_program_id_unknown();
-  test_instruction_program_id_stake();
-  test_instruction_program_id_system();
+    test_instruction_validate_ok();
+    test_instruction_validate_bad_program_id_index_fail();
+    test_instruction_validate_bad_first_account_index_fail();
+    test_instruction_validate_bad_last_account_index_fail();
+    test_instruction_program_id_unknown();
+    test_instruction_program_id_stake();
+    test_instruction_program_id_system();
 
-  printf("passed\n");
-  return 0;
+    printf("passed\n");
+    return 0;
 }
